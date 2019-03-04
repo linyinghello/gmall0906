5fc218d22ec39d8d3cef474b1186b1342e757968
linyinghello <linyinghello@aliyun.com>
2019-03-04 16:47:01 +0800

    最后一次111

--- gmall-mamage-web/src/test/java/com/linying/gmall/mamage/GmallMamageWebApplicationTests.java
+++ gmall-mamage-web/src/test/java/com/linying/gmall/mamage/GmallMamageWebApplicationTests.java
@@ -11,6 +11,7 @@ public class GmallMamageWebApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 		System.out.println(1);
 	}
 
--- gmall-mamage/src/main/java/com/linying/gmall/mamage/Service/Impl/AttrInfoServiceImpl.java
+++ gmall-mamage/src/main/java/com/linying/gmall/mamage/Service/Impl/AttrInfoServiceImpl.java
@@ -13,6 +13,8 @@ import java.util.List;
 @Service
 public class AttrInfoServiceImpl implements AttrInfoService {
 
+
+
     @Autowired
     private AttrInfoMapper attrInfoMappe;
 
